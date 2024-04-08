99c2c86764d49d5a267a497591a8c5d855a9ee57
kaJseia <89431075+mur4i@users.noreply.github.com>
2024-04-08 15:23:10 -0300

    update: mri_Qbox

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -22,8 +22,9 @@ server_scripts {
 shared_scripts {
     'shared/config.lua',
     '@qb-core/shared/locale.lua',
-    -- '@ox_lib/init.lua', -- OX_Lib, only line this in if you have ox_lib and are using them.
-    'locales/en.lua', -- English Locales
+    '@ox_lib/init.lua', -- OX_Lib, only line this in if you have ox_lib and are using them.
+    'locales/pt-br.lua', -- PT-BR Locales
+    -- 'locales/en.lua', -- English Locales
     -- 'locales/de.lua', -- German / Deutsch Locales
     -- 'locales/fr.lua', -- French / Français Locales
     -- 'locales/es.lua', -- Spanish / Español / Española Locales
@@ -41,15 +42,15 @@ dependencies { -- Make sure these are started before cdn-fuel in your server.cfg
     'PolyZone',
     'interact-sound',
     -- QB-Core Functionality (Input, Target, Menu)
-    'qb-target',
-    'qb-input',
-    'qb-menu',
+    -- 'qb-target',
+    -- 'qb-input',
+    -- 'qb-menu',
     -- QBox | Overextended Functionalities (Input, Progressbar, Target, Menu etc.)
-    -- 'ox_lib', -- Ox Library
-    -- 'ox_target',
+    'ox_lib', -- Ox Library
+    'ox_target',
 }
 
-data_file 'DLC_ITYP_REQUEST' 'stream/[electric_nozzle]/electric_nozzle_typ.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/[electric_charger]/electric_charger_typ.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[electric_nozzle]/electric_nozzle_typ.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/[electric_charger]/electric_charger_typ.ytyp'
 
 provide 'cdn-syphoning' -- This is used to override cdn-syphoning(https://github.com/CodineDev/cdn-syphoning) if you have it installed. If you don't have it installed, don't worry about this. If you do, we recommend removing it and using this instead.
